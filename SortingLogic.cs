2ae6a440783ad096556133ad30aa8465bf393605
Sebastian Oehm <sebastianoehm2001@gmail.com>
2023-01-22 22:36:57 +0100

    Projektdateien hinzufügen.

---
+++ SortingLogic.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteroo
+{
+    internal class SortingLogic
+    {
+        static public int[] Sorter(int[] arr)
+        {
+            int i = 0;
+            int x = 0;
+            int y = 0;
+            while (i < arr.Length - 1)
+            {
+                if (arr[i] > arr[i++])
+                {
+                    x = arr[i];
+                    y = arr[i + 1];
+                    arr[i] = y;
+                    arr[i + 1] = x;
+                }
+                i++;
+            }
+            return arr;
+        }
+        public static bool DoesContain(int[] arr, int number) 
+        {
+            bool answer = false;
+            foreach (int item in arr)
+            {
+                if (item == number) { answer = true; }
+            }
+            return answer;
+        }
+    }
+}
---
+++ TextLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteroo
+{
+    internal class TextLogic
+    {
+        public static int inputToInt()
+        {
+            int result = 0;
+            bool success = false;
+            while (!success)
+            {
+                try
+                {
+                    Console.Write("\nEnter a (non-decimal) number \n>");
+                    result = int.Parse(Console.ReadLine());
+                    success = true;
+                    return result;
+                }
+                catch
+                {
+                    Console.WriteLine("Error. Enter a valid number.");
+                    continue;
+                }
+            }
+            return result;
+        }
+    }
+}
