e11eba0a2998e308ea68d5b6b2f5226c8de15f3d
Sebastian Oehm <sebastianoehm2001@gmail.com>
2023-01-22 22:36:57 +0100

    replaced dummy logic

---
+++ Main.cs
@@ -0,0 +1,29 @@
+﻿namespace Sorteroo
+{
+    internal class Sorting
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 8, 2, 5, 70, 42, 61, 50, 22 };
+            int[] array2 = SortingLogic.Sorter(array);
+            int c = 0;
+            foreach(int i in array) 
+            {
+                c++;
+                if(c < array.Length)
+                {
+                    Console.Write(i+", ");
+                } else { Console.Write(i); }
+                 
+            }
+            int check = TextLogic.inputToInt();
+            if(SortingLogic.DoesContain(array, check))
+            {
+                Console.WriteLine($"Array does contain \"{check}\".");  
+            } else 
+            { 
+                Console.WriteLine($"Array does NOT contain \"{check}\"."); 
+            }
+        }        
+    }
+}
---
+++ SortingLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorteroo
+{
+    internal class SortingLogic
+    {
+        static public int[] Sorter(int[] arr)
+        {
+            int i;
+            int temp = 0;
+            bool success = false;
+            while (!success) 
+            {
+                i = 0;
+                bool swap = false;
+                while (i < arr.Length - 1)
+                {
+                    if (arr[i] > arr[i+1])
+                    {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    swap = true;
+                    }
+                i++;
+                }
+                if (swap == false) { break; }
+            }
+            return arr;
+        }
+        public static bool DoesContain(int[] arr, int number) 
+        {
+            bool answer = false;
+            foreach (int item in arr)
+            {
+                if (item == number) { answer = true; }
+            }
+            return answer;
+        }
+    }
+}
